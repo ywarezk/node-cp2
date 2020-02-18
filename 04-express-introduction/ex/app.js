d928029a028163df4b5a9fe80a385369bd1eba49
unknown <ywarezk@gmail.com>
2020-02-18 16:10:08 +0200

    almost finished with the entire api of the ex

--- 04-express-introduction/ex/app.js
+++ 04-express-introduction/ex/app.js
@@ -10,23 +10,32 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api/users", function(req, res) {
-    const users = userService.getUsers();
-    // res.send("put text")
-    
-    // res.send
-    res.status(200).json(users);
-});
+app.route("/api/users")
+    .get(function(req, res) {
+        const users = userService.getUsers();
+        // res.send("put text")
+        
+        // res.send
+        res.status(200).json(users);
+    })
+    .post(function(req, res) {
+        const userJson = req.body;
+        const user = new User(userJson)
+        userService.addUser(user);
+        res.status(201).json(user);
+    });
 
-// {}
-// req.body
-app.post("/api/users", function(req, res) {
-    // firstName = <script></script>
-    const userJson = req.body;
-    const user = new User(userJson)
-    userService.addUser(user);
-    res.status(201).json(user);
-})
+app.route("/api/users/:id")
+    .get(function(req, res) {
+        const user = userService.getSingleUser(req.params.id);
+        res.json(user);
+    })
+    .put(function(req, res) {
+        
+    })
+    .delete(function(req, res) {
+
+    });
 
 module.exports = app;
 
--- 04-express-introduction/ex/services/user.service.js
+++ 04-express-introduction/ex/services/user.service.js
@@ -5,6 +5,11 @@
 class UserService {
     constructor() {
         this.users = [];
+
+        this.getUsers = this.getUsers.bind(this);
+        this.getSingleUser = this.getSingleUser.bind(this);
+        this.updateUser = this.updateUser.bind(this);
+        this.addUser = this.addUser.bind(this);
     }
 
     getUsers() {
@@ -13,6 +18,12 @@ class UserService {
 
     getSingleUser(id) {
         return this.users.find((singUser) => singUser.id == id);
+    }
+
+    updateUser(user) {
+        const userToUpdate = this.users.find((singleUser) => singleUser.id == user.id);
+        Object.assign(userToUpdate, user);
+        return userToUpdate;
     }
 
     addUser(user) {
